80542b69277d38c9ba24d2d2eeb2158eb7c27012
wilbrone <wilbroneokoth@gmail.com>
2019-09-24 22:08:46 +0300

    practs

---
+++ js/scripts.js
@@ -0,0 +1,87 @@
+
+// THIS IS FOR FORM CONTROL GETTING DATA USING JQUERY.
+// $("#blanks form").submit(function() {
+//     var person1Input = $("input#person1").val();
+//         var person2Input = $("input#person2").val();
+//         var animalInput = $("input#animal").val();
+//         var exclamationInput = $("input#exclamation").val();
+//         var verbInput = $("input#verb").val();
+//         var nounInput = $("input#noun").val();
+
+//         $(".person1").text(person1Input);
+//         $(".person2").text(person2Input);
+//         $(".animal").text(animalInput);
+//         $(".exclamation").text(exclamationInput);
+//         $(".verb").text(verbInput);
+//         $(".noun").text(nounInput);
+
+//     $("#story").show();
+
+//     event.preventDefault();
+// });
+
+
+
+// THIS IS FOR EVENTS AND EVENT LISTENERS.
+// $("h1").click(function(){
+//     $("h1").toggle();
+//     $("h3").toggle();
+// });
+// $("h3").click(function(){
+//     $("h3").toggle();
+//     $("h1").toggle();
+// });
+
+
+
+
+// FOR CLICK EVENTS TO OPEN THE MODEL.
+// $("div#click-one").click(function(event) {
+//   whatToSay = "Hello!";
+//   alert(whatToSay);
+// });
+
+// $("div#click-two").click(function(event) {
+//   alert(whatToSay);
+// });
+
+
+
+// FOR CHANGING THE PAGE THEME WITH EVERY CLICK.
+// $("button#green").click(function() {
+//   $("body").removeClass();  
+//   $("body").addClass("green-background");
+// });
+
+// $("button#yellow").click(function() {
+//   $("body").removeClass();  
+//   $("body").addClass("yellow-background");
+// });
+
+// $("button#red").click(function() {
+//   $("body").removeClass();  
+//   $("body").addClass("red-background");
+// });
+
+
+// $("button#origin").click(function() {
+//   $("body").removeClass();  
+// });
+
+
+
+
+
+
+// FOR APPENDING AND PREPENDING ITEMS.
+// $("button#hello").click(function() {
+//   $("h2").prepend("Hello!");
+// });
+
+// $("button#goodbye").click(function() {
+//   $("ul").prepend("<li>Goodbye!</li>");
+// });
+
+// $("button#stop").click(function() {
+//   $("ul").prepend("<li>Stop copying me!</li>");
+// });
